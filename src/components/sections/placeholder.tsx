e0e4329c9b231e70ea9b5bf204eb864eb0c4c9e8
Bassey Duke <bassey.bd@gmail.com>
2023-06-02 00:25:33 -0400

    added extra padding for the placeholder page

--- src/components/sections/placeholder.tsx
+++ src/components/sections/placeholder.tsx
@@ -5,7 +5,7 @@ import { faArrowLeftLong } from "@fortawesome/free-solid-svg-icons";
 
 export default function Placeholder() {
   return (
-    <div className="relative text-white w-full h-full bg-black flex flex-col justify-center items-center">
+    <div className="relative text-white w-full h-full bg-black flex flex-col justify-center items-center pb-60">
       <div className="ont-extrabold text-3xl p-20">Coming soon!</div>
       <Link to="/">
         <FontAwesomeIcon
@@ -13,7 +13,7 @@ export default function Placeholder() {
           className="hover:text-blue-500 text-3xl"
         />
       </Link>
-      <img src={afroVibeCheck} alt="afroVibeCheck"></img>
+      <img src={afroVibeCheck} alt="afroVibeCheck" />
       <div>Version 1.0.0</div>
     </div>
   );
